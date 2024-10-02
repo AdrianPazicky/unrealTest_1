cebab7937c4a6b10dc613281dfde2ae55fbe6e6b
pihik <adrian1pazicky@gmail.com>
2024-10-02 16:49:51 +0200

    Added SCharacter and Setup Input
    - Added Character class.
    - Added MoveForward and Turn input

---
+++ Source/unrealTest_1Editor.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class unrealTest_1EditorTarget : TargetRules
+{
+	public unrealTest_1EditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+
+		ExtraModuleNames.AddRange( new string[] { "unrealTest_1" } );
+	}
+}
